f9e09a536d5b4f87451b2f29abe3821ee88e8af3
azzamaflah <azzamhebat46@gmail.com>
2023-03-06 11:47:05 +0700

    first commit

---
+++ latihan1.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main () {
+    // Begin
+    // numeric Luas,p,l
+
+    double Luas, p, l;
+
+    //display 'masukan Panjangnya= '
+    cout << "Masukan Panjangnya = ";
+    // accept p
+    cin >> p;
+    //display 'masukan lebarnya = '
+    cout << "Masukan Lebarnya = ";
+    //accept l
+    cin >> l;
+
+
+   
+    //end
+
+    
+
+}
